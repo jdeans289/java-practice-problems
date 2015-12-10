09636c649b4a9af7523631bbd79e8819ee0002c8
Jacqueline Deans <jdeans289@gmail.com>
2015-12-10 11:34:33 -0600

    Create Dragons.java

---
+++ CS1/Solutions/Dragons.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Dragons
+{
+	public static void main (String[] args) throws Exception
+	{
+		Scanner scan = new Scanner (new File ("dragons.in"));
+		while (scan.hasNextLine())
+		{
+			System.out.println(scan.nextLine());
+		}
+	}
+}
