cfe6dfc1ebcb52c6efa62b951c2d1f258686fd5f
Jacqueline Deans <jdeans289@gmail.com>
2015-12-10 11:35:30 -0600

    Update Dragons.java

--- CS1/Solutions/Dragons.java
+++ CS1/Solutions/Dragons.java
@@ -6,7 +6,7 @@ public class Dragons
 	public static void main (String[] args) throws Exception
 	{
 		Scanner scan = new Scanner (new File ("dragons.in"));
-		while (scan.hasNextLine())
+		while (scan.hasNext())
 		{
 			System.out.println(scan.nextLine());
 		}
