875463391a362a0bb434c7977e6dc55b2bd9dce5
Jacqueline Deans <jdeans289@gmail.com>
2015-12-11 08:47:51 -0600

    Update Crawl.java

--- CS1/Solutions/Crawl.java
+++ CS1/Solutions/Crawl.java
@@ -5,7 +5,7 @@ public class Crawl
 {
     public static void main (String[] args) throws Exception
     {
-        Scanner scan = new Scanner(new File("crawl.dat"));
+        Scanner scan = new Scanner(new File("crawl.in"));
         while(scan.hasNext())
         {
             System.out.println(scan.nextLine());
